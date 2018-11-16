5f37c8738003f78c39bed1ab72611ebe0bc60d84
Camm66 <Cam.morales93@gmail.com>
2018-11-15 21:48:20 -0600

    feat: translation for single words

--- TranslatorApp/src/app/dashboard/dashboard.component.ts
+++ TranslatorApp/src/app/dashboard/dashboard.component.ts
@@ -19,13 +19,8 @@ export class DashboardComponent implements OnInit {
 
   search(){
   this.wikipediaService.search(this.searchText)
-    .subscribe((res: any) => this.searchForPage(res));
-  }
-
-  searchForPage(res){
-    console.log(res);
-    this.wikipediaService.searchForPage(res)
-      .subscribe((res: any) => this.renderWikiResults(res));
+    .subscribe((res: any) => this.wikipediaService.searchForPage(res)
+      .subscribe((res: any) => this.renderWikiResults(res)));
   }
 
   renderWikiResults(res){
@@ -49,7 +44,7 @@ export class DashboardComponent implements OnInit {
     this.googleTranslateService.translate(text).subscribe((res: any) => {
            console.log(res);
            translation = res.data.translations[0].translatedText;
-           divElement.outerHTML = `<p>${translation}</p>`;
+           this.renderTranslatedText(item, translation);
          },
          err => {
            console.log(err);
@@ -58,7 +53,32 @@ export class DashboardComponent implements OnInit {
       //this.hisoryService.updateHistory("User translated text.", text, translation);
   }
 
-  addHistory(){
+  renderTranslatedText(item, translation){
+    var text;
+    var translatedText = translation.split(" ");
+    for(var i = 0; i < translatedText.length; i++){
+      var id = i + 10000
+      var word = `<span id="${id}" (click)="onClick(translateWord(${id}))"> ${translatedText[i]}</span>`
+      text += word;
+    }
+      var divElement = document.getElementById(item);
+      divElement.outerHTML = `<p>${text}</p>`;
+  }
+
+  translateWord(id){
+    console.log("Translate Word was Pressed");
+    var divElement = document.getElementById(id);
+    var text = divElement.innerHTML;
+    var translatedWord;
+    this.googleTranslateService.translate(text).subscribe((res: any) => {
+           console.log(res);
+           translatedWord = res.data.translations[0].translatedText;
+           divElement.outerHTML =  `<span id="${id}" (click)="onClick(translateWordBack(${id}))"> ${translatedWord}</span>`
+         },
+         err => {
+           console.log(err);
+         }
+       );
   }
 
   ngOnInit() {
--- TranslatorApp/src/app/google-translate.service.ts
+++ TranslatorApp/src/app/google-translate.service.ts
@@ -2,25 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { googleApiKey } from '../environments/environment'
 
+const url = `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}`;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GoogleTranslateService {
-  const url = `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}`;
   constructor(private http: HttpClient) {}
 
-  translate(text: any, key: string) {
+  translate(text: any) {
     var obj = new GoogleObj;
     obj.q = text
     console.log('Translating...');
-    return this.http.post(this.url, obj);
+    return this.http.post(url, obj);
   }
 }
+
   export class GoogleObj {
     q: string;
     readonly source: string = 'en';
     readonly target: string = 'es';
     readonly format: string = 'text';
-  }
 }
